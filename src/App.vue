ca8e66eef975ab9c7aa63431441fa00f5c1ba2d2
Vladislav Trofimov <70838553+viladit@users.noreply.github.com>
2024-02-23 22:50:00 +0300

    init commit without back connection

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <Header v-bind="headerParams"/>
+
+    <Loader id="load" v-if="loading"/>
+    <div id="contentContainer" v-else>
+      <router-view/>
+      <notifications group="error" position="bottom right" max="5" />
+      <notifications group="success" position="bottom right" max="5" />
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Loader from "@/components/Loader";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Loader
+  },
+  data(){
+    return {
+      headerParams: {
+        title: "Лабораторная работа №4",
+        authorName1: "Трофимов Владислав",
+        authorUrl1: "https://github.com/viladit",
+        authorName2: "Боринский Игорь",
+        authorUrl2: "https://github.com/xmnlssv",
+        varUrl: "https://github.com/viladit/webLab4"
+      },
+      footerParams: {
+        authorLogoPath1: "/assets/vlad.png",
+        authorUrl1: "https://github.com/viladit",
+        authorLogoPath2: "/assets/igot.png",
+        authorUrl2: "https://github.com/xmnlssv",
+      },
+      loading: true
+    }
+  },
+  mounted(){
+    setTimeout(() => {this.loading = false}, 1000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: whitesmoke;
+}
+
+#contentContainer{
+  margin-top: 100px;
+  margin-bottom: 90px;
+}
+#load{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  color: grey;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="header">
+      <div>
+        <span class="lab-name">{{title}}</span>
+      </div>
+      <div>
+        <span>
+          <a target="_blank" v-bind:href="authorUrl1">{{authorName1}}</a>
+          , P3214
+        </span>
+        <span>
+          <a target="_blank" v-bind:href="authorUrl2">{{authorName2}}</a>
+          , P3214
+        </span>
+        <span>
+          Вариант
+          <a target="_blank" v-bind:href="varUrl">2475</a>
+        </span>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    authorName1: String,
+    authorUrl1: String,
+    authorName2: String,
+    authorUrl2: String,
+    varUrl: String
+  }
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=MuseoModerno&display=swap");
+
+header{
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  height: 25%;
+  min-width: 440px;
+  display: flex;
+  background-color: aqua;
+  justify-content: space-between;
+  box-shadow: 0 0 20px 5px #009090;
+  font-family: "MuseoModerno", cursive;
+}
+
+/*
+  Название проекта:
+*/
+
+.header .lab-name{
+  color: whitesmoke;
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-top: 10px;
+}
+
+/*
+  Имена, Фамилии, вариант лабы:
+*/
+
+.header > div {
+  display: flex;
+  align-items: center;
+  margin: 5px 20px 5px;
+  flex-direction: column;
+}
+
+.header span, .header a{
+  color: white;
+  font-size: 0.9em;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+.header a:hover {
+  color: blue;
+  font-size: 0.95em;
+  transition: 0.5s;
+  text-shadow: 0 0 20px blue;
+  text-decoration: none;
+}
+</style>
